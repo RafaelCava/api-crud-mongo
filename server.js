6159c62a411e43beee237f20ca8fb1e6bc30b597
Rafael <ra.facavalcante@hotmail.com>
2021-08-10 15:50:07 -0300

    server: documentando

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+/* *
+* Arquivo: server.js
+* Descrição: 
+* Author:
+* Data de criação: 10/08/2021 
+*/
+
 const express = require('express');
 const { routes } = require('./routes/routes');
 const app = express()
