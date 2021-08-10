abb2fea99113e35b7df2da52ee55e5f75c800f53
Rafael <ra.facavalcante@hotmail.com>
2021-08-10 16:30:57 -0300

    app/models/produto: criando Schema

--- app/models/produto.js
+++ app/models/produto.js
@@ -4,3 +4,12 @@
 * Author: Rafael Cavalcante
 * Data de criação: 10/08/2021 
 */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const ProdutoSchema = new Schema({
+  Nome: String,
+  Preco: Number,
+  descricao: String
+})
