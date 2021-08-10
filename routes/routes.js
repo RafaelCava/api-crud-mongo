64e43c89b39b5435464a9d01c27b4d6b2f9ff085
Rafael <ra.facavalcante@hotmail.com>
2021-08-10 15:47:48 -0300

    routes/routes:

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const routes = express()
 
+routes.get('/api/produtos')
+routes.post('/api/produtos')
+routes.get('/api/produtos/:produtos_id')
+routes.put('/api/produtos/:produtos_id')
+routes.delete('/api/produtos/:produtos_id')
+
 module.exports = {
   routes
 }
