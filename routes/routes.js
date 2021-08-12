cdece3a2fee5366fb17930b79a69f341708407f1
Rafael <ra.facavalcante@hotmail.com>
2021-08-12 11:42:13 -0300

    routes/routes:

--- routes/routes.js
+++ routes/routes.js
@@ -5,22 +5,30 @@
  * Data de criação: 10/08/2021
  */
 const express = require('express')
-const { criarProduto, getProdutos, getProdutoById, substituirById, deleteById } = require('../controllers/controllers')
+const {
+  criarProduto,
+  getProdutos,
+  getProdutoById,
+  substituirById,
+  deleteById
+} = require('../controllers/controllers')
 const routes = express()
 
 routes.get('/', (req, res) => {
-  res.json({ message: 'Beleza! bem-vindo a loja' })
+  res.json({
+    message: 'Beleza! bem-vindo a loja'
+  })
 })
 
 routes.route('/produtos')
-.get(getProdutos)
-.post(criarProduto)
+  .get(getProdutos)
+  .post(criarProduto)
 
 
 routes.route('/produtos/:produto_id')
-.get(getProdutoById)
-.put(substituirById)
-.delete(deleteById)
+  .get(getProdutoById)
+  .put(substituirById)
+  .delete(deleteById)
 
 module.exports = {
   routes,
