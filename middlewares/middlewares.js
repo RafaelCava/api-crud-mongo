c46ed69faa8ae679af6283fc55bf1c6d718ace98
Rafael <ra.facavalcante@hotmail.com>
2021-08-12 00:57:37 -0300

    removendo body-parser

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -20,7 +20,23 @@ const post = (req, res, next) => {
   }
   next()
 }
+const put = (req, res, next) => {
+  if(req.method === 'PUT'){
+    if(!req.body.nome){
+      res.status(401).json({message: 'falta o parâmetro nome'})
+    }
+    if(!req.body.preco){
+      res.status(401).json({message: 'falta o parâmetro preço'})
+    }
+    if(!req.body.descricao){
+      res.status(401).json({message: 'falta o parâmetro descrição'})
+    }
+    next()
+  }
+  next()
+}
 
 module.exports = {
-  post
+  post,
+  put
 }
--- server.js
+++ server.js
@@ -6,11 +6,10 @@
  */
 
 const express = require('express')
-const bodyParser = require('body-parser')
 const swaggerUi = require('swagger-ui-express');
 const mongoose = require('mongoose');
 const { routes } = require('./routes/routes')
-const { post } = require('./middlewares/middlewares');
+const { post, put } = require('./middlewares/middlewares');
 const uri = "mongodb+srv://dbAdmin:12345@node-crud-api.fog8d.mongodb.net/node-crud-api";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true})
 
@@ -18,12 +17,11 @@ const app = express()
 
 const port = process.env.port || 8000
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use(post)
+app.use(put)
 
 app.use('/api', routes)
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(require('./swagger/swagger.json')))
