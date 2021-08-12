aa3aab5b9c681582d7af333bad37a1f8b6eae48a
Rafael <ra.facavalcante@hotmail.com>
2021-08-12 10:14:39 -0300

    finalizando

--- server.js
+++ server.js
@@ -7,18 +7,29 @@
 
 const express = require('express')
 const swaggerUi = require('swagger-ui-express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const { routes } = require('./routes/routes')
-const { post, put } = require('./middlewares/middlewares');
+const {
+  routes
+} = require('./routes/routes')
+const {
+  post,
+  put
+} = require('./middlewares/middlewares');
 const uri = "mongodb+srv://dbAdmin:12345@node-crud-api.fog8d.mongodb.net/node-crud-api";
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 const app = express()
 
 const port = process.env.port || 8000
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(bodyParser.json())
 
 app.use(post)
 app.use(put)
